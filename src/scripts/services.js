83acd86feb737063f89ff31f0ca154374f84dc2d
Jad229 <jad229@njit.edu>
2023-04-28 22:44:16 -0400

    feat: services page added

---
+++ src/scripts/services.js
@@ -0,0 +1,53 @@
+export const haircuts = [
+  {
+    name: "Women's Haircut and Blowout",
+    price: "$60",
+  },
+  {
+    name: "Men's Haircut and Beard Trim",
+    price: "$40",
+  },
+  {
+    name: "Blowout and Styling",
+    price: "$50",
+  },
+  {
+    name: "Updos and Formal Styles",
+    price: "$80",
+  },
+  {
+    name: "Hair Extensions and Weaves",
+    price: "$150",
+  },
+  {
+    name: "Keratin Treatment",
+    price: "$200",
+  },
+];
+
+export const colors = [
+  {
+    name: "Single Process Color",
+    price: "$100",
+  },
+  {
+    name: "Highlights and Lowlights",
+    price: "$150",
+  },
+  {
+    name: "Balayage and Ombre",
+    price: "$200",
+  },
+  {
+    name: "Color Correction",
+    price: "$250",
+  },
+  {
+    name: "Gloss and Toner",
+    price: "$75",
+  },
+  {
+    name: "Creative Color (e.g. pastels, neon, etc.)",
+    price: "$300",
+  },
+];
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,23 +1,49 @@
 <script>
 import ServiceCard from "../components/ServiceCard.vue";
+import { haircuts, colors } from "../scripts/services";
+
 export default {
   name: "Services",
   components: { ServiceCard },
+  computed: {
+    haircuts() {
+      return haircuts;
+    },
+    colors() {
+      return colors;
+    },
+  },
 };
 </script>
 <template>
   <section class="p-6 space-y-6 max-w-7xl mx-auto">
     <div class="bg-gray-100 py-6">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="max-w-7xl space-y-24 mx-auto px-4 sm:px-6 lg:px-8">
         <div class="text-center">
           <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">
             Haircuts and Styling
           </h2>
         </div>
-        <div class="mt-10 flex justify-center">
-          <ServiceCard />
-          <ServiceCard />
-          <ServiceCard />
+        <div class="space-x-6 mt-10 flex justify-center">
+          <ServiceCard
+            v-for="(haircut, index) in haircuts"
+            :key="index"
+            :service="haircut"
+            class="w-full"
+          />
+        </div>
+        <div class="text-center">
+          <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+            Colors and Treatments
+          </h2>
+        </div>
+        <div class="space-x-6 mt-10 flex justify-center">
+          <ServiceCard
+            v-for="(color, index) in colors"
+            :key="index"
+            :service="color"
+            class="w-full"
+          />
         </div>
       </div>
     </div>
